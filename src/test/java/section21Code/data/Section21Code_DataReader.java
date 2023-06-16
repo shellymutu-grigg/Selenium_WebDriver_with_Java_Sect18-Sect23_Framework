840753bcbd2d5921fee59d4b3ba422c5d3579eff
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-06-17 01:19:51 +1200

    Section 21 Code fix error

--- src/test/java/section21Code/data/Section21Code_DataReader.java
+++ src/test/java/section21Code/data/Section21Code_DataReader.java
@@ -1,18 +1,20 @@
 package section21Code.data;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Section21Code_DataReader {
 
-	public List<HashMap<String, String>> getJsonDataToMap() {
+	public List<HashMap<String, String>> getJsonDataToMap() throws IOException {
 
 		// Read the JSON file
 		String jsonContentString = FileUtils.readFileToString(
@@ -21,9 +23,14 @@ public class Section21Code_DataReader {
 
 		// Convert String to HashMap
 		ObjectMapper mapper = new ObjectMapper();
-		List<Hashmap<String, String>> dataHashMap = mapper.readValue(jsonContentString,
-				new TypeReference<List<Hashmap<String, String>>>() {
-				});
+		// List<HashMap<String, String>> dataHashMap =
+		// mapper.readValue(jsonContentString,
+		// new TypeReference<List<HashMap<String, String>>>() {
+		// });
+
+		List<HashMap<String, String>> dataHashMap = new Gson().fromJson(jsonContentString,
+				new TypeToken<List<HashMap<String, String>>>() {
+				}.getType());
 
 		return dataHashMap;
 	}
